e00825ec95cf766be82c801dfb51e4e0283aa613
Eduardo Apata <eduardotito.jose01@gmail.com>
2024-09-06 15:20:38 -0400

    style(sidenav ): arreglando modelo

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -44,5 +44,5 @@ export const routes: Routes = [
         ],
       },
     ]
-  }
+  },{ path: '**', redirectTo: '' }
 ];
--- Frontend/src/app/features/dashboard/dashboard.component.ts
+++ Frontend/src/app/features/dashboard/dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, inject, OnInit, signal } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatCardModule } from '@angular/material/card';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -6,7 +6,7 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatMenuModule } from '@angular/material/menu';
-import { SidenavAyudantesComponent } from './layout/sidenav-ayudantes/sidenav-ayudantes/sidenav-ayudantes.component';
+import { SidenavAyudantesComponent } from './layout/sidenav-ayudantes/sidenav-ayudantes.component';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { NgOptimizedImage } from '@angular/common'
 
@@ -27,63 +27,19 @@ import { NgOptimizedImage } from '@angular/common'
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css',
 })
-export default class DashboardComponent implements OnInit {
-
+export default class DashboardComponent implements OnInit{
+ 
 
   public breakpointObserver = inject(BreakpointObserver);
 
-  public collapsed = signal(false);
   public isMobile = signal(false);
 
-  public profilePicture = computed(() => {
-    return this.collapsed() ? '30px' : '100px';
-  });
-
-  public sideNavWidth = computed(() => {
-    if (this.isMobile()) {
-      if(this.collapsed()){
-        return '0';
-      }else{
-        return '12rem';
-      }
-    }else {
-      if(this.collapsed() ){
-        return '3.75rem';
-      }else{
-        return '14rem';
-      }
-    }
-  });
-
-  public modeSideNav = computed(() => {
-    if (this.isMobile()) {
-      if(this.collapsed()){
-        return 'side';
-      }else{
-        return 'over';
-      }
-    }else {
-      return 'side'
-    }
-  })
-
-  public contentWidth = computed(() => {
-    if (this.isMobile()) {
-      return '0';
-    }else {
-      if(this.collapsed()){
-        return '3.75rem';
-      }else{
-        return '14rem';
-      }
-    }
-  });
 
   ngOnInit(): void {
-    this.breakpointUpdate();
+    this.breakpointUpdate();  
   }
 
-  breakpointUpdate(){
+  private breakpointUpdate(){
     this.breakpointObserver
     .observe([
       Breakpoints.XSmall,
@@ -93,18 +49,15 @@ export default class DashboardComponent implements OnInit {
     .subscribe((result) => {
       if(result.breakpoints[Breakpoints.XSmall] ){
         console.log('MOBILE');
-        this.collapsed.set(true);
         this.isMobile.set(true);
       }
-      else if( result.breakpoints[Breakpoints.Large]){
-        console.log('PC');
-        this.collapsed.set(false);
-        this.isMobile.set(false);
-      }
-      else if( result.breakpoints[Breakpoints.Small]){
+      else if(result.breakpoints[Breakpoints.Small]){
         console.log('Tablet');
-        this.collapsed.set(true);
         this.isMobile.set(true);
+
+      }else if(result.breakpoints[Breakpoints.Large]){
+        console.log('PC');
+        this.isMobile.set(false);
       }
     });
   };
